5f568d689294a11364226894c26fb76ec19685f5
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2020-11-17 04:27:41 -0800

    Update name

--- ascii_fish.fish
+++ ascii_fish.fish
@@ -1,1 +1,1 @@
-curl https://raw.githubusercontent.com/garryspins/ASCII-Art-Splash-Screen/master/art/(shuf -i 1-10 | head -1).txt
+curl https://raw.githubusercontent.com/garryspins/ASCII-Art-Splash-Screen-Fish/master/art/(shuf -i 1-10 | head -1).txt
